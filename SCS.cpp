372ed95d0a38bcce6e865bb1fb4fa2a34a375011
Alvis Logins <allogn@gmail.com>
2016-04-03 00:27:14 +0300

    globalH added, no incremental addition of edges

--- SCS.cpp
+++ SCS.cpp
@@ -50,80 +50,80 @@ void SCS::startAugment(int max_length) {
             // rerun dijkstra since all visited nodes were updated
 
             fHeap<LargeCost> dijkH;
-//            fHeap<LargeCost> globalH;
+            fHeap<LargeCost> globalH;
             vector<int> visited;
             vector<int> parent(_res_node_num, -1); //parent node and an edge to a child
             LargeCost mindist[_res_node_num];
-//            if (QryCnt[start] < _graph.fullE[start].size())
-//                globalH.enqueue(start, 0);
+            if (QryCnt[start] < _graph.fullE[start].size())
+                globalH.enqueue(start, 0);
 
             //do not update every time
 
             LargeCost curcost;
             int u, tip;
-//            do {
-//
-//                if (globalH.size() > 0) {
-//                    int fromid;
-//                    LargeCost tmp;
-//                    globalH.dequeue(fromid,tmp);
-//                    int eid = _graph.fullE[fromid][QryCnt[fromid]];
-//                    QryCnt[fromid]++;
-//                    if (QryCnt[fromid] < _graph.fullE[fromid].size()) {
-//                        globalH.enqueue(fromid, mindist[fromid] + _graph.E[_graph.fullE[fromid][QryCnt[fromid]]].weight);
+            do {
+
+                if (globalH.size() > 0) {
+                    int fromid;
+                    LargeCost tmp;
+                    globalH.dequeue(fromid,tmp);
+                    int eid = _graph.fullE[fromid][QryCnt[fromid]];
+                    QryCnt[fromid]++;
+                    if (QryCnt[fromid] < _graph.fullE[fromid].size()) {
+                        globalH.enqueue(fromid, mindist[fromid] + _graph.E[_graph.fullE[fromid][QryCnt[fromid]]].weight);
+                    }
+
+                    uintT local_eid = _res_arc_num;
+                    //for spatial data this does not work
+                    _first_out[fromid].push_back(local_eid);
+                    int toid = _graph.get_pair(eid, fromid);
+                    _first_out[toid].push_back(local_eid + 1);//reverse edge
+                    _arc_idf.push_back(local_eid);
+                    _arc_idb.push_back(local_eid + 1);
+                    _forward.push_back(true);
+                    _forward.push_back(false);
+                    _source.push_back(fromid);
+                    _source.push_back(toid);
+                    _target.push_back(toid);
+                    _target.push_back(fromid);
+                    //skipped lower
+                    _upper.push_back(_graph.E[eid].capacity);
+                    _upper.push_back(_graph.E[eid].capacity);
+                    _scost.push_back(_graph.E[eid].weight);
+                    _scost.push_back(-_graph.E[eid].weight);
+                    LargeCost lc =
+                            static_cast<LargeCost>(_scost[local_eid]) * _res_node_num * _alpha; //COST MODIFICATION
+                    _cost.push_back(lc);
+                    _cost.push_back(-lc);
+                    // calculate new epsilon based on two potential values and cost (note the minus sign!)
+//                LargeCost new_epsilon = std::max(-(lc + _pi[toid] - _pi[fromid]), _epsilon);
+//                _epsilon = new_epsilon; //todo maybe this can be modified
+                    _res_cap.push_back(_upper[local_eid]);
+                    _res_cap.push_back(0);
+                    _reverse.push_back(local_eid + 1);
+                    _reverse.push_back(local_eid);
+
+                    _res_arc_num += 2;
+
+                    //update dijkH heap to continue dijkstra from the point it ended
+                    LargeCost rc = _cost[local_eid] - _pi[fromid] + _pi[toid]; //exactly +delta_pi because we increase potential
+                    LargeCost l = rc + _epsilon;
+                    assert(l >= 0); //epsilon-optimality
+                    //todo add visited array
+//                    assert(mindist[fromid] < std::numeric_limits<LargeCost>::max()); //because if it was placed in global => it was visited
+//                    cout << "adding " << fromid << "->" << toid << endl;
+//                    if (mindist[toid] > mindist[fromid] + l) {
+//                        //check if exists in a heap
+//                        if (!dijkH.isExisted(toid)) {
+//                            dijkH.enqueue(toid, mindist[fromid] + l);
+//                        } else {
+//                            dijkH.updatequeue(toid, mindist[fromid] + l);
+//                        }
+//                        parent[toid] = local_eid;
+//                        mindist[toid] = mindist[fromid] + l;
+//                        cout << "updated distance " << mindist[toid] << endl;
 //                    }
-//
-//                    uintT local_eid = _res_arc_num;
-//                    //for spatial data this does not work
-//                    _first_out[fromid].push_back(local_eid);
-//                    int toid = _graph.get_pair(eid, fromid);
-//                    _first_out[toid].push_back(local_eid + 1);//reverse edge
-//                    _arc_idf.push_back(local_eid);
-//                    _arc_idb.push_back(local_eid + 1);
-//                    _forward.push_back(true);
-//                    _forward.push_back(false);
-//                    _source.push_back(fromid);
-//                    _source.push_back(toid);
-//                    _target.push_back(toid);
-//                    _target.push_back(fromid);
-//                    //skipped lower
-//                    _upper.push_back(_graph.E[eid].capacity);
-//                    _upper.push_back(_graph.E[eid].capacity);
-//                    _scost.push_back(_graph.E[eid].weight);
-//                    _scost.push_back(-_graph.E[eid].weight);
-//                    LargeCost lc =
-//                            static_cast<LargeCost>(_scost[local_eid]) * _res_node_num * _alpha; //COST MODIFICATION
-//                    _cost.push_back(lc);
-//                    _cost.push_back(-lc);
-//                    // calculate new epsilon based on two potential values and cost (note the minus sign!)
-////                LargeCost new_epsilon = std::max(-(lc + _pi[toid] - _pi[fromid]), _epsilon);
-////                _epsilon = new_epsilon; //todo maybe this can be modified
-//                    _res_cap.push_back(_upper[local_eid]);
-//                    _res_cap.push_back(0);
-//                    _reverse.push_back(local_eid + 1);
-//                    _reverse.push_back(local_eid);
-//
-//                    _res_arc_num += 2;
-//
-//                    //update dijkH heap to continue dijkstra from the point it ended
-//                    LargeCost rc = _cost[local_eid] - _pi[fromid] + _pi[toid]; //exactly +delta_pi because we increase potential
-//                    LargeCost l = rc + _epsilon;
-//                    assert(l >= 0); //epsilon-optimality
-//                    //todo add visited array
-////                    assert(mindist[fromid] < std::numeric_limits<LargeCost>::max()); //because if it was placed in global => it was visited
-////                    cout << "adding " << fromid << "->" << toid << endl;
-////                    if (mindist[toid] > mindist[fromid] + l) {
-////                        //check if exists in a heap
-////                        if (!dijkH.isExisted(toid)) {
-////                            dijkH.enqueue(toid, mindist[fromid] + l);
-////                        } else {
-////                            dijkH.updatequeue(toid, mindist[fromid] + l);
-////                        }
-////                        parent[toid] = local_eid;
-////                        mindist[toid] = mindist[fromid] + l;
-////                        cout << "updated distance " << mindist[toid] << endl;
-////                    }
-//                }
+                }
 
                 dijkH.clear();
                 dijkH.enqueue(start,0);
@@ -144,9 +144,9 @@ void SCS::startAugment(int max_length) {
                         goto checkEdges;
                     }
                     dijkH.dequeue();
-//                    if (!globalH.isExisted(tip) && QryCnt[tip] < _graph.fullE[tip].size()) {
-//                        globalH.enqueue(tip, mindist[tip] + _graph.E[_graph.fullE[tip][QryCnt[tip]]].weight);
-//                    }
+                    if (!globalH.isExisted(tip) && QryCnt[tip] < _graph.fullE[tip].size()) {
+                        globalH.enqueue(tip, mindist[tip] + _graph.E[_graph.fullE[tip][QryCnt[tip]]].weight);
+                    }
                     visited.push_back(tip);
                     LargeCost rc, l;
                     LargeCost pi_tip = _pi[tip];
@@ -173,7 +173,7 @@ void SCS::startAugment(int max_length) {
                 }
                 //todo maybe we can clear dijkstra since everything else is definetly farther than corrent result?
                 checkEdges:;
-//            } while (globalH.size() > 0);
+            } while (globalH.size() > 0);
 
 
             // increase potentials for all visited nodes
