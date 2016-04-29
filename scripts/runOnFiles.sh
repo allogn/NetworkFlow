61f373caafbd2616affa2e5258c5185333086d23
Alvis Logins <allogn@gmail.com>
2016-04-29 18:32:27 +0300

    SIA vs newSIA added results on bipartite

--- scripts/runOnFiles.sh
+++ scripts/runOnFiles.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-DATADIR="../../data/bipartite_sparse"
+DATADIR="../../data/bipartite"
 for f in $DATADIR/*.gr;
 do
-	../Release/NetworkFlows -r 1 -i $f -a 2 -p 0 -l ../results/LDA_sparse_bipart.csv
-	../Release/NetworkFlows -r 1 -i $f -a 0 -p 0 -l ../results/SIA_sparse_bipart.csv
-	../Release/NetworkFlows -r 1 -i $f -a 5 -p 0 -l ../results/OLemon_sparse_bipart.csv
+	../Release/NetworkFlows -r 1 -i $f -a 0 -p 0 -l ../results/SIAvsSIAnew_on_bipart.csv
+	../Release/NetworkFlows -r 1 -i $f -a 0 -p 1 -l ../results/SIAvsSIAnew_on_bipart.csv
 done
 
 echo "Done."
